0034c01ee4efe76dd4af716406f04df29207c8fd
unknown <mikeromilcabiluna587@gmail.com>
2025-01-07 00:08:13 +0800

    first trial

---
+++ contact.php
@@ -0,0 +1,30 @@
+
+
+
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEUVILLETE</title>
+</head>
+<link rel="stylesheet" href="nuvi.css">
+<body>
+
+
+
+
+    <center>
+<form class="form">
+    <input class="input" type="text" placeholder="Name">
+    <input class="input" type="text" placeholder="E-Mail I.D.">
+    <textarea class="textarea" placeholder="Enter message"></textarea> 
+    <center><button class="button">Submit</button></center>
+</form></center>
+
+
+</body>
+</html>
---
+++ neuvilette.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEUVILETTE</title>
+    <link rel="stylesheet" href="nuvi.css">
+</head>
+
+<body>
+
+
+<!-- background loop -->
+<div class="vid-container">
+        <video autoplay muted loop class="back">
+         <source src="assets\img\archon.mp4">
+        </video>
+</div>
+
+<!-- hamburger button -->
+    <label class="bar" for="check">
+    <input type="checkbox" id="check">
+    <span class="top"></span>
+    <span class="middle"></span>
+    <span class="bottom"></span>
+    </label>
+
+
+
+
+
+</body>
+</html>
